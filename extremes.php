018d26d3b551cf2a50d376d0f012f8718e0a9357
ashu_12_34 <1705178@kiit.ac.in>
2020-01-31 23:01:57 +0530

    second commit

--- extremes.php
+++ extremes.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
+  <title>Registration system PHP and MySQL </title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
